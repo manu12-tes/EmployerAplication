b7695315af968624955bf2116ab21426f92087a0
manu12-tes <kiramanuel123@gmail.com>
2024-02-08 01:43:10 -0600

    Agregar archivos de proyecto.

---
+++ EmployerAplication/Controllers/PruebaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployerAplication.Controllers
+{
+    public class PruebaController : Controller
+    {
+
+        // GET: /HelloWorld/
+        [HttpGet("/HelloWorld")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        [HttpGet("/HelloWorld/Welcome")]
+       public IActionResult Welcome(String name , int numTimes=1)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["numTimes"] = numTimes;
+            return View();
+        }
+    }
+}
---
+++ EmployerAplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployerAplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
